875608bdf37e74246ddf8ed3a790f568c8321a2a
TomasCipriano <d47173704@alumnos.itr.edu.ar>
2024-06-20 16:40:37 -0300

    navbar lista *revisar css

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('home.html')
+
+@app.route('/partidos')
+def partidos():
+    return render_template('partidos.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
